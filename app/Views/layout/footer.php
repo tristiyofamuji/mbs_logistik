38217834893384650eb2257d9065c7b088617794
fikicahyanto <fikicahyanto96@gmail.com>
2022-07-01 11:42:07 +0700

    s

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 <!-- BEGIN btn-scroll-top -->
 <a href="#" data-toggle="scroll-to-top" class="btn-scroll-top fade"><i class="fa fa-arrow-up"></i></a>
-=======
-
-		<!-- END #content -->
-		<!-- BEGIN btn-scroll-top -->
-		<a href="#" data-toggle="scroll-to-top" class="btn-scroll-top fade"><i class="fa fa-arrow-up"></i></a>
->>>>>>> 6412fa03a0bfbdb2dc736d381acc0e07f05de8a0
 		<!-- END btn-scroll-top -->
 	</div>
 	<!-- END #app -->
