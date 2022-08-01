7254fa1d13a860faf77b4aaff67e0da5dde59bd7
fikicahyanto <fikicahyanto96@gmail.com>
2022-08-01 09:55:53 +0700

    update fiki gajisopir

--- app/Models/SopirModel.php
+++ app/Models/SopirModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class SopirModel extends Model
 {
     protected $table = "t_sopir";
-    protected $allowedFields = ['id','nama','telepon','no_whatsapp','alamat','no_kendaraan'];
+    protected $allowedFields = ['id','id_pemilik','nama','telepon','no_whatsapp','alamat','no_kendaraan'];
 
     public function getGaji()
     {
